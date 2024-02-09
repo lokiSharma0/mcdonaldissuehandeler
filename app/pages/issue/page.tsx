351116be40c93031f8ae694eda63e512b8cba448
luffyJoyboy <lorenzsharma@gmail.com>
2024-02-10 10:41:42 +1100

    radix-ui

--- app/pages/issue/page.tsx
+++ app/pages/issue/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
 const Issuepage = () => {
   return (
     <div>
       <h1>This is issue page</h1>
+      <Button>New issue</Button>
     </div>
   );
 };

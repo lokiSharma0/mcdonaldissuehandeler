7654701bf48525f35b36651b33be753d6ffcb3e0
luffyJoyboy <lorenzsharma@gmail.com>
2024-02-10 10:54:39 +1100

    buildthenewissuepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <NavBar />
-          {children}
+          <main className="px-8">{children}</main>
         </Theme>
       </body>
     </html>
--- app/pages/issue/page.tsx
+++ app/pages/issue/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 const Issuepage = () => {
   return (
     <div>
       <h1>This is issue page</h1>
-      <Button>New issue</Button>
+
+      <Button>
+        <Link href="/pages/issue/newIssue">New Issue</Link>
+      </Button>
     </div>
   );
 };
